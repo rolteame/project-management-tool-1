4ce822b74f21c7be13ae689b9a5d31f0b2f4441b
bankymono <bankymono@gmail.com>
2020-10-20 02:11:19 +0100

    Tasks page functionalities

--- assets/js/Controllers/team.js
+++ assets/js/Controllers/team.js
@@ -1,11 +1,17 @@
+// CURRENT USER GOTTEN FROM LOCAL STORAGE
+currentUser = JSON.parse(localStorage.getItem("currentUser"));
+
 // USERS LIST GOTTEN FROM LOCAL STORAGE
 users = JSON.parse(localStorage.getItem("users"));
+
+// TEAMS LIST GOTTEN FROM LOCAL STORAGE
 teams = JSON.parse(localStorage.getItem('teams'));
 if (teams == null || teams == undefined) {
     teams = [];
 }
 
-
+// ##############################[   funtion displayUsers begins here   ]##################################
+// FUNCTION TO DISPLAY USERS ON TEAMS PAGE
 function displayUsers(){
     usersPlaceholder = "";
 
@@ -16,6 +22,7 @@ function displayUsers(){
     }
     document.getElementById('usersList').innerHTML = usersPlaceholder
 }
+//##############################[ Function Display Users Ends Here  ] ########################################
 
 // function listSelected(){
 //     console.log(document.getElementById("usersList").value)
@@ -27,24 +34,37 @@ function displayUsers(){
 
 //     // document.getElementById('showSelected').append(usersPlaceholder)
 // }
-    teamDisplayDiv =  document.getElementById("showSelected")
+
+
+// SELECTS div of selected users for team 
+teamDisplayDiv =  document.getElementById("showSelected")
+
+//#####################[ Function addUp begins here ]#############################
+// appends users to div to add to team. Called in displayUsers function above
 function addUp(e){
     
     displayDivChildren = teamDisplayDiv.children 
     console.log(displayDivChildren)
 // console.log(e.target.selected,e.target.value)
     if(e.target.selected == true){
+
             innerDiv = document.createElement('div')
+            button = document.createElement('button')
+            button.innerHTML ='X'
+            button.onclick = function (e){
+                this.parentElement.parentElement.removeChild(this.parentElement)
+            }
             innerDiv.append(e.target.value)
+            innerDiv.append(button)
             innerDiv.id = e.target.id
             teamDisplayDiv.appendChild(innerDiv)
     }
-    else{
-        toRemove = document.getElementById(e.target.id)
-        teamDisplayDiv.removeChild(toRemove)       
-    }
+    
 }
+// #############[     Function addUp ends Here ]###############################################
 
+// #############[     Function createTeam begins Here ]###############################################
+// It is called in team.html 
 let teamId;
 function createTeam(){
     for (let i = 0; i <= teams.length; i++) {
@@ -58,6 +78,8 @@ function createTeam(){
 
     for(i=0;i<displayDivChildren.length;++i){
         teamList.push(displayDivChildren[i].id)
+        users[displayDivChildren[i].id].teamIdList.push(teamId)
+
     }
 
     let newTeam ={
@@ -68,9 +90,10 @@ function createTeam(){
 
     teams.push(newTeam)
     localStorage.setItem('teams', JSON.stringify(teams))
-    location.href="taskoverview.html"
+    localStorage.setItem('users', JSON.stringify(users))
+    location.href="temp.html"
 }
-
+// #############[     Function createTeam ends Here   ]###############################################
 teamTable = [
     {
         teamID: 0,
