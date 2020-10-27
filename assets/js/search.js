8d2b2651e908ff114e745041cd7b18f2edf5b4bf
eonyedikachi <ibeabuchionyedikachi@gmail.com>
2020-10-27 22:19:41 +0100

    search function update

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,5 +1,6 @@
 
     // PROJECTS LIST GOTTEN FROM LOCAL STORAGE
+<<<<<<< HEAD
 projects = JSON.parse(localStorage.getItem("projects"));
 if (projects == null || projects == undefined) {
   projects = [];
@@ -85,3 +86,97 @@ searchInput.addEventListener('input', (event) => {
         clearList();
     }
 }); 
+=======
+    projects = JSON.parse(localStorage.getItem("projects"));
+    if (projects == null || projects == undefined) {
+      projects = [];
+    }
+    
+    
+    const projectList = getProjectsName(projects);
+    
+    
+    const searchList = document.getElementgitById("searchList");
+    
+    //save projects name from localStorage to a list
+    function getProjectsName(projects){
+        let list = [];
+            for (let i = 0; i < projects.length; i++) {
+                list[i] = {name: projects[i].projectName.toLowerCase(), projectId: projects[i].projectId};
+            }
+            
+            return list;
+    }
+
+    console.log(projectList);
+    
+    //set searchList from localStorage
+    function setList(group){
+        clearList();
+        for (const searchTerm of group){
+            const item = document.createElement("li");
+            item.classList.add("list-group-item","search-display");
+            const text = document.createTextNode(searchTerm.name);
+            item.appendChild(text);
+            searchList.appendChild(item);
+
+            // searchChoice = document.getElementsByClassName(search)
+        }
+
+        console.log(searchList);
+        if(group.length === 0){
+            setNoResults();
+        }
+    
+    }
+    
+    //clear searchList so we dont have duplicates 
+    function clearList(){
+        while(searchList.firstChild){
+            searchList.removeChild(searchList.firstChild);
+        }
+    }
+    
+    
+    //set invalid search response
+    function setNoResults(){
+        const item = document.createElement("li");
+        item.classList.add("list-group-item");
+        const text = document.createTextNode("No results found");
+        item.appendChild(text);
+        searchList.appendChild(item);
+    }
+    
+    //get relevant of search input
+    function getRelevancy(value, searchTerm){
+        if (value === searchTerm){
+            return 2;
+        } else if (value.startsWith(searchTerm)){
+            return 1;
+        } else if (value.includes(searchTerm)){
+            return 0;
+        } else{
+            return -1;
+        }
+    }
+    
+    const searchInput = document.getElementById("search");
+    
+    // main search action is performed here
+    searchInput.addEventListener('input', (event) => {
+        let value = event.target.value;
+    
+        if(value && value.trim().length > 0){
+            value = value.trim().toLowerCase();
+            setList(projectList.filter(searchTerm => {
+                return searchTerm.name.includes(value);
+            }).sort((searchTermA, searchTermB) => {
+                return getRelevancy(searchTermB.name, value)   -  getRelevancy(searchTermA.name, value);
+            }));
+        } else {
+    
+            clearList();
+        }
+    }); 
+    
+>>>>>>> 9cb438492bbaa455f3182a88845859f4a04fda59
