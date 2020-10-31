611ca439e032687073acd2c96a97a38f1e3bdc5b
Chidinma Nwabekee <chibekee17@gmail.com>
2020-10-31 04:10:10 -0700

    Notify

--- assets/notification.js
+++ assets/notification.js
@@ -1,3 +1,32 @@
-let tasks = [];
+
+// tasks = localStorage.getItem("tasksLists");
+// document.getElementById("notify").innerHTML = tasks
+// task[0].name
+const Task = JSON.parse(localStorage.getItem("tasksLists"));
+const Notification = JSON.parse(localStorage.getItem("Notification"));
+if (Notification == null || Notification == undefined) {
+    Notification = [];
+}
+
+ 
+// function displayTask() {
+//   let  notify = "";
+//     for (i = 0; i < task.length; i++) {
+//       if (task != null || task != undefined) {
+//         notify += `<div>
+//             <strong>Name</strong> : ${task[i].name}
+//         </div> `;
+//       }
+//     }
+//     document.getElementById("notify").innerHTML = notify
+
+    
+//   }
+//   displayTask()
+
+function saveNotification() { 
+    
+
+
+}
   
-document.getElementById("notify").innerHTML = localStorage.getItem("tasksLists");
